8a5b0679b5c0076a8b1a2d7b4450e9a05eed2e79
otuti Code <arthur.renato.nv@gmail.com>
2025-03-16 12:44:19 -0300

    ❇️ feat -  Nova atv20.03 SOLID

--- Autenticação 13.03/Cliente.java
+++ Autenticação 13.03/Cliente.java
@@ -13,8 +13,10 @@ public class Cliente extends Pessoa implements Autenticavel, Departamento {
     public Cliente(String nome, int idade, String sexo, String endereco, String telefone) {
             super(nome, idade, sexo, endereco, telefone);
         }
-    
-        @Override
+        azer uma implementação em uma linguagem Orientada JAva a Objetos para cada princípio de programação SOLID (SRP, OCP, LSP, ISP e DIP ). a implementação inicialmente deve apresentar o código em desacordo com o princípio. Você deve mostrar o que está ferindo o princípio, explicando por meio de comentários no código e depois deve apresentar o código corrigido e que esteja de acordo com o princípio. O código incorreto deve ser colocado numa classe e o código correto em outra classe. 
+
+        E deve ser feito um relatório explicando o que faz cada código implementado e seu respectivo princípio. A explicação deve conter o porquê uma classe "errada" viola o princípio e oo porquê a "classe certa" não viola o princípio.
+    @Override
     public void Login() {
         // Implementação do login para Cliente
         System.out.println("Cliente fez login.");
@@ -25,6 +27,7 @@ public class Cliente extends Pessoa implements Autenticavel, Departamento {
         // Implementação do criarDepartamento para Cliente
         System.out.println("Cliente criou departamento.");
     }
+    
     @Override
     public void cadastrarFuncionario() {
         // Implementação do cadastrarFuncionario para Cliente
